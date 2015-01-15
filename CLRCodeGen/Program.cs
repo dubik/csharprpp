976d2402d4142d0d517f97cb0db52121c6297a27
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-01-15 22:46:37 +0200

    Added project to solution

---
+++ CLRCodeGen/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CLRCodeGen
+{
+    internal class ICodegenContext
+    {
+        
+    }
+
+    internal class Node
+    {
+        public void Analyze()
+        {
+        }
+
+        public void Codegen()
+        {
+        }
+    }
+
+    internal class Program
+    {
+        private static void DoSomething()
+        {
+            AssemblyName assemblyName = new AssemblyName("sampleAssembl");
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".dll");
+
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("Program", TypeAttributes.Public);
+
+            ConstructorBuilder constructorBuilder = typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+            Type ty = typeBuilder.CreateType();
+            assemblyBuilder.Save(assemblyName.Name + ".dll");
+        }
+
+        private static void CreateExe()
+        {
+            AssemblyName assemblyName = new AssemblyName("sampleAssembl");
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".exe");
+
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("Program", TypeAttributes.Public);
+
+            MethodBuilder mainMethod = typeBuilder.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, CallingConventions.Standard,
+                typeof (int),
+                new[] {typeof (String[])});
+            ILGenerator il = mainMethod.GetILGenerator();
+            il.UsingNamespace("System");
+            il.EmitWriteLine("Moikka");
+            il.Emit(OpCodes.Ret);
+
+            typeBuilder.CreateType();
+
+            assemblyBuilder.SetEntryPoint(mainMethod.GetBaseDefinition());
+            assemblyBuilder.Save(assemblyName.Name + ".exe", PortableExecutableKinds.Required32Bit, ImageFileMachine.I386);
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Moika");
+        }
+
+        private static void Main()
+        {
+            CreateExe();
+        }
+    }
+}
