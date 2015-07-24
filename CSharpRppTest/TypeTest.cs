56aaac33b82b9383639d6978e026a88cdefc2975
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-07-24 16:07:09 +0300

    Added TypeTest

---
+++ CSharpRppTest/TypeTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpRppTest
+{
+    [TestClass]
+    class TypeTest
+    {
+        private Type k = typeof (int);
+
+        [TestMethod]
+        public void TEST()
+        {
+            
+        }
+    }
+}
