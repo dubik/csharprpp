62dbae847a057631676ee231f9375f298ceba873
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-08-29 21:07:05 +0300

    Updated C# version of monad pattern, added 'map' function

--- RppRuntime/Closures.cs
+++ RppRuntime/Closures.cs
@@ -2,21 +2,50 @@
 
 namespace RppRuntime
 {
-    public class Option<T>
+    public abstract class Option<T>
     {
+        public abstract bool IsEmpty { get; }
+        public abstract T Get { get; }
+
+        public Option<B> Map<B>(Func<T, B> f)
+        {
+            if (IsEmpty)
+            {
+                return new None<B>();
+            }
+
+            return new Some<B>(f(Get));
+        }
     }
 
     public class Some<T> : Option<T>
     {
-        public T Item;
+        public Some(T val)
+        {
+            Get = val;
+        }
+
+        public override bool IsEmpty => false;
+        public override T Get { get; }
+    }
+
+    public class None<T>: Option<T>
+    {
+        public override bool IsEmpty => true;
+
+        public override T Get
+        {
+            get { throw new Exception("Is empty"); }
+        }
     }
 
     public class Main
     {
         public static int main()
         {
-            Some<int> k = new Some<int> {Item = 123};
-            return k.Item;
+            var k = new Some<int>(123);
+            k.Map(x => x * 2);
+            return k.Get;
         }
     }
 
