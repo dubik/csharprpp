3f1b26e3714aa6754e012b89f482feb02e6a1fae
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-09-26 21:39:13 +0300

    Added several generic tests

--- CSharpRppTest/GenericsTest.cs
+++ CSharpRppTest/GenericsTest.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RppRuntime;
 
 namespace CSharpRppTest
 {
@@ -22,5 +21,75 @@ object Bar
             var res = Utils.InvokeStatic(barTy, "main", new object[] {"hello"});
             Assert.AreEqual("hello", res);
         }
+
+        [TestMethod]
+        public void SimpleGenericClass()
+        {
+            const string code = @"
+class Foo[T](val id: T)
+
+object Bar
+{
+    def main : Foo[Int] = new Foo[Int](10)
+}
+";
+            Type barTy = Utils.ParseAndCreateType(code, "Bar$");
+            var res = Utils.InvokeStatic(barTy, "main");
+            Assert.IsNotNull(res);
+        }
+
+        [TestMethod]
+        public void ExtendingSpecializedGeneric()
+        {
+            const string code = @"
+class Foo[T](val id: T)
+
+class SecondFoo(id: Int) extends Foo[Int](id)
+
+object Bar
+{
+    def main : Foo[Int] = new SecondFoo(10)
+}
+";
+            Type barTy = Utils.ParseAndCreateType(code, "Bar$");
+            var res = Utils.InvokeStatic(barTy, "main");
+            Assert.IsNotNull(res);
+        }
+
+        [TestMethod]
+        public void ExtendingSpecializedGenericAndDefineOneMoreGenericParameter()
+        {
+            const string code = @"
+class Foo[T](val id: T)
+
+class SecondFoo[A](id: Int, val name: A) extends Foo[Int](id)
+
+object Bar
+{
+    def main : Foo[Int] = new SecondFoo[String](10, ""Hello"")
+}
+";
+            Type barTy = Utils.ParseAndCreateType(code, "Bar$");
+            var res = Utils.InvokeStatic(barTy, "main");
+            Assert.IsNotNull(res);
+        }
+
+        [TestMethod]
+        public void ClosureLikeClass()
+        {
+            const string code = @"
+class Func[R, T]
+{
+    def apply(arg : T) : R
+}
+
+class MyClosure extends Func[Int, Float]
+{
+    def apply
+}
+
+";
+        }
+
     }
 }
