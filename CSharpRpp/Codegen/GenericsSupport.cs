bb01ba3b96128eb6d5b729535173814a1850cd06
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-10-08 15:24:21 +0300

    Removed duplicated code in GenericsSupport

--- CSharpRpp/Codegen/GenericsSupport.cs
+++ CSharpRpp/Codegen/GenericsSupport.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using JetBrains.Annotations;
@@ -10,22 +11,22 @@ namespace CSharpRpp.Codegen
         // TODO typeParams are updated, we should find a better way, can't just pass list of objects and poke their properies
         public static void DefineGenericParams([NotNull] IEnumerable<RppVariantTypeParam> typeParams, MethodBuilder funcBuilder)
         {
-            IEnumerable<RppVariantTypeParam> variantTypeParams = typeParams as IList<RppVariantTypeParam> ?? typeParams.ToList();
-            if (variantTypeParams.Any())
-            {
-                var genericParams = variantTypeParams.Select(x => x.Name).ToArray();
-                GenericTypeParameterBuilder[] genericTypeBuilders = funcBuilder.DefineGenericParameters(genericParams);
-                variantTypeParams.ForEachWithIndex((index, param) => param.Runtime = genericTypeBuilders[index].AsType());
-            }
+            DefineGenericParams(typeParams, funcBuilder.DefineGenericParameters);
         }
 
         public static void DefineGenericParams([NotNull] IEnumerable<RppVariantTypeParam> typeParams, [NotNull] TypeBuilder classType)
+        {
+            DefineGenericParams(typeParams, classType.DefineGenericParameters);
+        }
+
+        private static void DefineGenericParams([NotNull] IEnumerable<RppVariantTypeParam> typeParams,
+            [NotNull] Func<string[], GenericTypeParameterBuilder[]> genericParamFactory)
         {
             IEnumerable<RppVariantTypeParam> variantTypeParams = typeParams as RppVariantTypeParam[] ?? typeParams.ToArray();
             if (variantTypeParams.Any())
             {
                 var genericParams = variantTypeParams.Select(x => x.Name).ToArray();
-                GenericTypeParameterBuilder[] genericTypeBuilders = classType.DefineGenericParameters(genericParams);
+                GenericTypeParameterBuilder[] genericTypeBuilders = genericParamFactory(genericParams);
                 variantTypeParams.ForEachWithIndex((index, param) => param.Runtime = genericTypeBuilders[index].AsType());
             }
         }
