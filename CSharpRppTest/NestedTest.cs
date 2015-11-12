129d8f731964c4ec210388c9dae97420fa6df0b6
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-11-12 16:09:02 +0200

    Added test for invoking function in nested class

--- CSharpRppTest/NestedTest.cs
+++ CSharpRppTest/NestedTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSharpRppTest
@@ -20,6 +21,30 @@ class Bar {
             Type[] nestedTypes = barTy.GetNestedTypes();
             Assert.AreEqual(1, nestedTypes.Length);
             Assert.AreEqual("Internal", nestedTypes[0].Name);
+        }
+
+        [TestMethod]
+        public void CallFunctionFromNestedClass()
+        {
+            const string code = @"
+class Bar {
+    class Internal {
+        def func : Int = 13
+    }
+
+    def getValue : Int = {
+        val inst = new Internal()
+        inst.func()
+    }
+}
+";
+
+            var barTy = Utils.ParseAndCreateType(code, "Bar");
+            Assert.IsNotNull(barTy);
+            object barInst = Activator.CreateInstance(barTy);
+            MethodInfo getValueFunc = barTy.GetMethod("getValue");
+            object res = getValueFunc.Invoke(barInst, null);
+            Assert.AreEqual(13, res);
         }
     }
 }
