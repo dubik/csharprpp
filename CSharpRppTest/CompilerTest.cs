510675dc54670843785db7bb51de708e2549f117
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-12-01 23:16:17 +0200

    Added test for spawning rppcompiler and peverify

---
+++ CSharpRppTest/CompilerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpRppTest
+{
+    [TestClass]
+    public class CompilerTest
+    {
+        [TestMethod]
+        public void SpawnCompiler()
+        {
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                FileName = "CSharpRpp.exe",
+                CreateNoWindow = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process csharProcess = Process.Start(info);
+            Assert.IsNotNull(csharProcess);
+
+            csharProcess.WaitForExit();
+            Assert.AreEqual(1, csharProcess.ExitCode);
+
+            ProcessStartInfo peverifyInfo = new ProcessStartInfo
+            {
+                FileName = "peverify.exe",
+                CreateNoWindow = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process proc = Process.Start(peverifyInfo);
+            Assert.IsNotNull(proc);
+            proc.WaitForExit();
+            Assert.AreEqual(1, csharProcess.ExitCode);
+        }
+    }
+}
