578c8872c43d7e0d2215b81483007ac5b29141ce
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2015-12-05 20:06:45 +0200

    Added project for compiling from string

---
+++ BufferCompiler/Program.cs
@@ -0,0 +1,59 @@
+﻿using CSharpRpp;
+using CSharpRpp.Codegen;
+using CSharpRpp.Reporting;
+
+namespace BufferCompiler
+{
+    class Program
+    {
+        private static void Main()
+        {
+            /*
+            const string code = @"
+abstract class List[A] {
+    def head : A
+    def tail : List[A]
+    def isEmpty : Boolean
+}
+
+object Nil extends List[Nothing]
+{
+    override def head: Nothing = throw new Exception(""Empty list"")
+    override def tail: List[Nothing] = throw new Exception(""Empty list"")
+    override def isEmpty : Boolean = true
+}
+
+class Cons[A](val _head: A, val _tail: List[A]) extends List[A]
+{
+    override def head: A = _head
+    override def tail: List[A] = _tail
+    override def isEmpty : Boolean = true
+}
+
+object Cons
+{
+    def doSome[A](x: A) : A = x
+    def apply[A](head: A, tail: List[A]) : List[A] = new Cons[A](head, tail)
+}
+
+object Main
+{
+    def main() : Int = {
+        Cons.doSome[Int](13)
+    }
+}
+";
+*/
+            const string code1 = @"
+object Main
+{
+    def main(argv: Array[String]) : Int = {
+        13
+    }
+}";
+            Diagnostic diagnostic = new Diagnostic();
+            CodeGenerator codeGen = RppCompiler.Compile(program => RppCompiler.Parse(code1, program), diagnostic, "Sample.dll");
+            codeGen.Save("Sample.dll");
+        }
+    }
+}
--- CSharpRpp/Program.cs
+++ CSharpRpp/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System.Linq;
 using CommandLine;
-using CSharpRpp.Codegen;
 using CSharpRpp.Reporting;
 
 namespace CSharpRpp
 {
     public class Program
     {
-        public static int Main(string[] args)
+        public static int Main1(string[] args)
         {
             var result = CommandLine.Parser.Default.ParseArguments<RppOptions>(args);
             return result.MapResult(RunAndReturnExitCode, _ => 1);
@@ -25,54 +24,6 @@ namespace CSharpRpp
                 return 1;
             }
 
-            return 0;
-        }
-
-        public static int Main1(string[] args)
-        {
-            const string code = @"
-abstract class List[A] {
-    def head : A
-    def tail : List[A]
-    def isEmpty : Boolean
-}
-
-object Nil extends List[Nothing]
-{
-    override def head: Nothing = throw new Exception(""Empty list"")
-    override def tail: List[Nothing] = throw new Exception(""Empty list"")
-    override def isEmpty : Boolean = true
-}
-
-class Cons[A](val _head: A, val _tail: List[A]) extends List[A]
-{
-    override def head: A = _head
-    override def tail: List[A] = _tail
-    override def isEmpty : Boolean = true
-}
-
-object Cons
-{
-    def doSome[A](x: A) : A = x
-    def apply[A](head: A, tail: List[A]) : List[A] = new Cons[A](head, tail)
-}
-
-object Main
-{
-    def main() : Int = {
-        Cons.doSome[Int](13)
-    }
-}
-";
-            const string code1 = @"
-object Main
-{
-    def main() : Unit = {
-    }
-}";
-            Diagnostic diagnostic = new Diagnostic();
-            CodeGenerator codeGen = RppCompiler.Compile(program => RppCompiler.Parse(code1, program), diagnostic, "Sample.dll");
-            codeGen.Save("Sample.dll");
             return 0;
         }
     }
