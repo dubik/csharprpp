0ece6a6714e2c2e31e9ba6f478fbe658257488f3
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2016-03-12 18:12:25 +0200

    Added missing file

---
+++ RppRuntime/Console.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Console
+{
+    public static Console _instance = new Console();
+
+    public void println(string obj)
+    {
+        System.Console.WriteLine(obj);
+    }
+
+    public void printFormat(string format, params object[] args)
+    {
+        System.Console.WriteLine(format, args);
+    }
+}
