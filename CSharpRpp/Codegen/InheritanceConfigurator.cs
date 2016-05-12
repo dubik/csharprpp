4873901cfe3eceda7b88c62fce784d7063fab1a9
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2016-05-12 08:37:42 +0300

    Cleanup

--- CSharpRpp/Codegen/InheritanceConfigurator.cs
+++ CSharpRpp/Codegen/InheritanceConfigurator.cs
@@ -1,34 +1,11 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reflection.Emit;
-
-namespace CSharpRpp.Codegen
+﻿namespace CSharpRpp.Codegen
 {
-    public class InheritanceConfigurator : RppNodeVisitor
-    {
-        public override void VisitEnter(RppClass node)
-        {
-            if (node.BaseConstructorCall.BaseClassType2.Value.Name != "Object")
-            {
-                TypeBuilder builder = node.Type.NativeType as TypeBuilder;
-                Debug.Assert(builder != null, "builder != null");
-
-                //builder.SetParent(node.BaseConstructorCall.BaseClassType.Runtime);
-            }
-        }
-    }
-
     public class InheritanceConfigurator2 : RppNodeVisitor
     {
         public override void VisitEnter(RppClass node)
         {
             if (node.BaseConstructorCall.BaseClassType2.Value.Name != "Object")
             {
-                //TypeBuilder builder = node.RuntimeType as TypeBuilder;
-                //Debug.Assert(builder != null, "builder != null");
-
-                //builder.SetParent(node.BaseConstructorCall.BaseClassType.Runtime);
-
                 node.Type.BaseType = node.BaseConstructorCall.BaseClassType2.Value;
             }
         }
